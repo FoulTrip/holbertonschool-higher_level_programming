1e9441a07f4e2411dd9728a1d446fbca10169318
foultrip <davidvasquezmahecha@gmail.com>
2023-09-29 12:06:32 -0500

    act002

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-import sys
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    argLen = len(args)
+    import sys
     
-    if argLen == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif argLen == 1:
+    elif count == 1:
         print("1 argument:")
-        print(f"1: {args[0]}")
+    else:
+        print("{} arguments:".format(count))
+        
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
