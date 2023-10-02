aad1031c3aa479a570979f8fffb5021925280cde
foultrip <davidvasquezmahecha@gmail.com>
2023-10-02 11:27:50 -0500

    act004

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     if my_list:
-        my_list.reverse()
-        for number in my_list:
+        for number in my_list[::-1]:
             print("{:d}".format(number))
