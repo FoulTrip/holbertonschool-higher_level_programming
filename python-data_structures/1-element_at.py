95abfd4e50727af9b936694d66d0f5d141d09f15
foultrip <davidvasquezmahecha@gmail.com>
2023-10-02 10:35:26 -0500

    act002

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    lenList = len(my_list)
-    if idx >= lenList or idx < 0:
+    if idx >= len(my_list) or idx < 0:
         return None
     
     return my_list[idx]
