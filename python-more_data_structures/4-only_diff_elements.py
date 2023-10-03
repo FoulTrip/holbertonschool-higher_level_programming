68a58cb2067bcc7ef3a837e6d126971a585cf7ed
foultrip <davidvasquezmahecha@gmail.com>
2023-10-03 12:04:41 -0500

    act002

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    # operación de diferencia simétrica (^) para obtener elementos presentes en un conjunto pero no en ambos
+    diff = set_1 ^ set_2
+    return diff
