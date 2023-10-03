6b731a670ca8be565830680dda1a5813af4bfb05
foultrip <davidvasquezmahecha@gmail.com>
2023-10-03 13:15:55 -0500

    act003

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,8 +1,4 @@
-#!/urs/bin/python3
+#!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    keys = sorted(a_dictionary.keys())
-
-    for key in keys:
-        value = a_dictionary[key]
-        print(f"{key}: {value}")
+    [print("{}: {}".format(k, a_dictionary[k])) for k in sorted(a_dictionary)]
