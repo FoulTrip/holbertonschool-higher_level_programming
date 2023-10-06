5cf26e4595ab07c70f0d98ac7059303bd67eb72b
foultrip <davidvasquezmahecha@gmail.com>
2023-10-06 12:28:54 -0500

    act003

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,5 +6,11 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         result = None
     finally:
-        print("Inside result: {}".format("{:d}".format(result) if result is not None else result))
+        if result is not None:
+            if isinstance(result, int):
+                print("Inside result: {:d}".format(result))
+            else:
+                print("Inside result: {:d}".format(int(result)))
+        else:
+            print("Inside result: {}".format(result))
         return result
