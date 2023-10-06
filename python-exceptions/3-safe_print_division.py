fef1db3cd506854dd143e56c6365d51b06d09112
foultrip <davidvasquezmahecha@gmail.com>
2023-10-06 12:13:23 -0500

    act002

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -20,4 +20,8 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             result_list.append(result)
+    
+    for r in result_list:
+        print("{:d}".format(int(r)))
+    
     return result_list
