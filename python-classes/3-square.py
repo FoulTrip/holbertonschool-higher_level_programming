2b2c53dc193b53313e029257a98845f31c6be5d0
foultrip <davidvasquezmahecha@gmail.com>
2023-10-12 12:47:15 -0500

    act003

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -23,4 +23,8 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = size**2
+        self.__size = size
+        
+    def area(self):
+        """current area of the square """
+        return (self.__size * self.__size)
