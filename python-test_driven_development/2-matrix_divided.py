b45fc850b681ddfefb10916434f9d21f3f398bf8
foultrip <davidvasquezmahecha@gmail.com>
2023-10-14 01:01:17 -0500

    act002

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,23 +6,30 @@
 
 
 def matrix_divided(matrix, div):
-    """Returns a new matrix
-    matrix must be a list of lists of integers or floats
-    """
-
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    if not all(isinstance(row, list) for row in matrix) or not all(
-        isinstance(element, (int, float)) for row in matrix for element in row
-    ):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-
-    if len(set(len(row) for row in matrix)) > 1:
-        raise TypeError("Each row of the matrix must have the same size")
-
-    new_matrix = [[round(element / div) for element in row] for row in matrix]
+    """Divide a matrix by a number div"""
+    list_error = "matrix must be a matrix (list of lists) of integers/floats"
+    len_error = "Each row of the matrix must have the same size"
+    div_int_error = "div must be a number"
+    div_zero_error = "division by zero"
+    new_matrix = []
+    new_list = []
+    if not matrix:
+        raise TypeError(list_error)
+    if type(div) is not int and type(div) is not float:
+        raise TypeError(div_int_error)
+    if div is 0:
+        raise ZeroDivisionError(div_zero_error)
+    longitud = len(matrix[0])
+    for lista in matrix:
+        if type(lista) is not list:
+            raise TypeError(list_error)
+        if len(lista) != longitud:
+            raise TypeError(len_error)
+        for item in lista:
+            if type(item) is not int and type(item) is not float:
+                raise TypeError(list_error)
+            num = item / div
+            new_list.append(round(num, 2))
+        new_matrix.append(new_list)
+        new_list = []
     return new_matrix
