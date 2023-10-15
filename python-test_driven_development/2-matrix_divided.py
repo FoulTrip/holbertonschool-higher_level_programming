b6b37b4dd8eda055f767ada0763a961402bf1244
foultrip <davidvasquezmahecha@gmail.com>
2023-10-14 21:44:09 -0500

    act002

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,25 +11,20 @@ def matrix_divided(matrix, div):
     len_error = "Each row of the matrix must have the same size"
     div_int_error = "div must be a number"
     div_zero_error = "division by zero"
-    new_matrix = []
-    new_list = []
-    if not matrix:
+
+    if not all(isinstance(row, list) for row in matrix) or not all(
+        isinstance(item, (int, float)) for row in matrix for item in row
+    ):
         raise TypeError(list_error)
-    if type(div) not in (int, float):
+
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError(len_error)
+
+    if not isinstance(div, (int, float)):
         raise TypeError(div_int_error)
+
     if div == 0:
         raise ZeroDivisionError(div_zero_error)
-    longitud = len(matrix[0])
-    for lista in matrix:
-        if not isinstance(lista, list):
-            raise TypeError(list_error)
-        if len(lista) != longitud:
-            raise TypeError(len_error)
-        for item in lista:
-            if not isinstance(item, (int, float)):
-                raise TypeError(list_error)
-            num = item / div
-            new_list.append(round(num, 2))
-        new_matrix.append(new_list)
-        new_list = []
+
+    new_matrix = [[round(item / div, 2) for item in row] for row in matrix]
     return new_matrix
