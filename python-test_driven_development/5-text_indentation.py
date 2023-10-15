3e6df96281c104e9d31025e8d6dd67e9e5b44904
foultrip <davidvasquezmahecha@gmail.com>
2023-10-15 01:58:41 -0500

    act005

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+    Text indentation
+"""
+
+def text_indentation(text):
+    """prints a text with 2 new lines after each of these characters: '.', '?' and ':' """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    new_text = ""
+    special_chars = {".", "?", ":"}
+
+    for char in text:
+        new_text += char
+
+        if char in special_chars:
+            new_text += "\n\n"
+
+    print("\n".join(line.strip() for line in new_text.split("\n")))
