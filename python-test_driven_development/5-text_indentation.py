d11b86e64d0a94728a8254d4448a6ee199152dff
foultrip <davidvasquezmahecha@gmail.com>
2023-10-15 02:09:36 -0500

    act005

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,18 +4,25 @@
     Text indentation
 """
 
+
 def text_indentation(text):
-    """prints a text with 2 new lines after each of these characters: '.', '?' and ':' """
-    if not isinstance(text, str):
+    """
+    prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if type(text) is not str:
         raise TypeError("text must be a string")
-
-    new_text = ""
-    special_chars = {".", "?", ":"}
-
-    for char in text:
-        new_text += char
-
-        if char in special_chars:
-            new_text += "\n\n"
-
-    print("".join(line.strip() for line in new_text.split("\n")))
+    esp_ch = ""
+    for a_char in text:
+        if esp_ch == "." or esp_ch == "?" or esp_ch == ":":
+            if a_char == " ":
+                continue
+            else:
+                esp_ch = ""
+                print(a_char, end="")
+                continue
+        if a_char == "." or a_char == "?" or a_char == ":":
+            print(a_char)
+            print("")
+            esp_ch = a_char
+        else:
+            print(a_char, end="")
