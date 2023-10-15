f912a6e353378389bade52c8fc290b3f0fa25b87
foultrip <davidvasquezmahecha@gmail.com>
2023-10-15 11:16:07 -0500

    act002

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -18,7 +18,7 @@ class Rectangle:
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
@@ -31,7 +31,7 @@ class Rectangle:
     def height(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        elif value <= 0:
             raise ValueError("width must be >= 0")
         else:
             self.__height = value
