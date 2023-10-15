aeecb78ec48d533d608fd753b1008e925c14cc36
foultrip <davidvasquezmahecha@gmail.com>
2023-10-15 02:30:54 -0500

    act006

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -18,15 +18,28 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_positive_values(self):
         self.assertEqual(max_integer([1, 3, 2, 4]), 4)
+        self.assertEqual(max_integer([1, 1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
 
     def test_negative_values(self):
         self.assertEqual(max_integer([-1, -3, -2, -4]), -1)
+        self.assertEqual(max_integer([-5, -5, -5, -5, -5]), -5)
+        self.assertEqual(max_integer([-1, -2, -3, -4, -5]), -1)
 
     def test_mixed_values(self):
         self.assertEqual(max_integer([-1, 3, 0, -4]), 3)
+        self.assertEqual(max_integer([0, -1, 2, -3, 4]), 4)
+        self.assertEqual(max_integer([-1, -2, 0, 2, 1]), 2)
 
     def test_duplicate_values(self):
         self.assertEqual(max_integer([1, 1, 1, 1]), 1)
+        self.assertEqual(max_integer([0, 0, 0, 0, 0]), 0)
+        self.assertEqual(max_integer([-2, -2, -2, -2, -2]), -2)
+
+    def test_list_of_one_element(self):
+        self.assertEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([-3]), -3)
+        self.assertEqual(max_integer([0]), 0)
 
 
 if __name__ == "__main__":
