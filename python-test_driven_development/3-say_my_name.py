cf9d147494da3b95d37b49b95f465b10263b26f1
foultrip <davidvasquezmahecha@gmail.com>
2023-10-15 00:58:21 -0500

    act003

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,6 +7,8 @@ Say my name
 
 def say_my_name(first_name, last_name=""):
     """ prints My name is <first name> <last name> """
-    if type(first_name) != str or type(last_name) != str:
+    if type(first_name) != str:
         raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
     print("My name is {} {}".format(first_name, last_name))
