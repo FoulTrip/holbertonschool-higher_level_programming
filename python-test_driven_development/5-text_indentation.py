539ac6793851d7e31a9ebff1644356e5b062e897
foultrip <davidvasquezmahecha@gmail.com>
2023-10-15 02:00:37 -0500

    act004

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -16,6 +16,6 @@ def text_indentation(text):
         new_text += char
 
         if char in special_chars:
-            new_text += "\n\n"
+            new_text += "\n"
 
     print("\n".join(line.strip() for line in new_text.split("\n")))
