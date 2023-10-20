3ba9d11be6bb525193df02d6af5d82ac9b500747
foultrip <davidvasquezmahecha@gmail.com>
2023-10-20 11:01:16 -0500

    act003

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -15,11 +15,6 @@ my_list = load_from_json_file(filename)
 print(my_list)
 print(type(my_list))
 
-filename = "my_dict.json"
-my_dict = load_from_json_file(filename)
-print(my_dict)
-print(type(my_dict))
-
 try:
     filename = "my_set_doesnt_exist.json"
     my_set = load_from_json_file(filename)
@@ -29,7 +24,7 @@ except Exception as e:
     print("[{}] {}".format(e.__class__.__name__, e))
 
 try:
-    filename = "my_fake.json"
+    filename = "my_dict.json"
     my_fake = load_from_json_file(filename)
     print(my_fake)
     print(type(my_fake))
