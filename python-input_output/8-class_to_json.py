ea88cc3a78a3d76d1694acc22527685a686df64c
foultrip <davidvasquezmahecha@gmail.com>
2023-10-21 12:27:26 -0500

    act006

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+"""Class to JSON """
+
 
 def class_to_json(obj):
+    """
+    returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    """
     dicc_obj = dict()
 
     for key in obj.__dict__:
--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,6 +4,8 @@
 
 
 class Student:
+    """defines a student"""
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
