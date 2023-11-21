b1260b177335cf78205e7b17847538269a6911b7
foultrip <davidvasquezmahecha@gmail.com>
2023-11-21 10:48:11 -0500

    act004.1

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,5 @@
 --insert value in a new row into the table `first_table`
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
+INSERT INTO
+    `first_table` (`id`, `name`)
+VALUES
+    (89, "Best School");
