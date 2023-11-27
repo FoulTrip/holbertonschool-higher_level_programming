15d935eaa97bae6d415bb82213755899009d3cbd
foultrip <davidvasquezmahecha@gmail.com>
2023-11-27 10:22:37 -0500

    act002

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,16 @@
+SELECT
+    `id`,
+    `name`
+FROM
+    `cities`
+WHERE
+    `state_id` IN (
+        SELECT
+            `id`
+        FROM
+            `states`
+        WHERE
+            `name` = "California"
+    )
+ORDER BY
+    `id`;
