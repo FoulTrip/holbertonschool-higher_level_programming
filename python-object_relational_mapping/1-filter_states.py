65978dd2efadf2207481ced66bc698e5483c29c8
FoulTrip <davidvasquezmahecha@gmail.com>
2023-12-01 03:13:42 +0100

    act003.2

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,11 +17,10 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states \
+    cur.execute("SELECT * FROM states \
                 WHERE name LIKE BINARY 'N%' \
-                ORDER BY states.id ASC"
-    )
+                ORDER BY states.id ASC")
+
     rows = cur.fetchall()
     
     for row in rows:
