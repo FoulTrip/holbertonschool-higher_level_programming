61ad6ac1cd29b9a9d81cb5054c14ef3f295ce37c
foultrip <davidvasquezmahecha@gmail.com>
2024-02-21 10:41:14 -0500

    act004.3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -6,15 +6,18 @@ class Rectangle:
     """defines a rectangle"""
 
     def __init__(self, width=0, height=0):
+        """Initializes the rectangle with optional width and height."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Initializes the rectangle with optional width and height."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Sets the width of the rectangle."""
         if type(value) != int:
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -24,10 +27,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Retrieves the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Sets the height of the rectangle."""
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
