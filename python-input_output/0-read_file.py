aad99f7ee0d9e8875a4f7e2e2f597a0672da370c
foultrip <davidvasquezmahecha@gmail.com>
2024-02-26 09:30:53 -0500

    act001.1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
+"""
+This script defines a function 'read_file' that reads and prints the contents of a file.
+"""
+
 
 def read_file(filename=""):
+    """
+    Reads the contents of a file and prints them.
+
+    Parameters:
+    - filename: The name of the file to read. Defaults to an empty string.
+
+    Returns: None
+    """
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
