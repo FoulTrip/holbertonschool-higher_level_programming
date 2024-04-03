bb9025fb4f8a539f24d2a3c0f1fe7b0a8d07b6b4
foultrip <davidvasquezmahecha@gmail.com>
2024-04-03 14:49:19 -0500

    mapping 04

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -5,9 +5,7 @@ from sys import argv
 
 if __name__ == "__main__":
     """
-    takes in an argument and displays all values
-    in the states table of hbtn_0e_0_usa where name
-    matches the argument.
+    lists all cities from the database hbtn_0e_4_usa
     """
 
     host = "localhost"
@@ -15,16 +13,17 @@ if __name__ == "__main__":
     user = argv[1]
     password = argv[2]
     database = argv[3]
-    state_name_searched = argv[4]
 
     db = MySQLdb.connect(
         host=host, port=port, user=user, password=password, db=database
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    states = cur.fetchall()
-    for state in states:
-        if state[1] == state_name_searched:
-            print("({}, \'{}\')".format(state[0], state[1]))
+    cur.execute("SELECT cities.id, cities.name, states.name \
+        FROM cities JOIN states ON cities.state_id = state.id\
+        ORDER BY cities.id ASC")
+    cities = cur.fetchall()
+    print(cities)
+    for city in cities:
+        print(city)
     cur.close()
     db.close()
