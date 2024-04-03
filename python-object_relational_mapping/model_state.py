79ee23432971d33baae62c0cdccf703bd00f184c
foultrip <davidvasquezmahecha@gmail.com>
2024-04-03 14:29:53 -0500

    mapping model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""Model state"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Class State"""
+
+    __tablename__ = "states"
+    id = Column(
+        Integer, 
+        primary_key=True, 
+        autoincrement=True, 
+        unique=True, 
+        nullable=False
+    )
+    name = Column(String(128), nullable=False)
