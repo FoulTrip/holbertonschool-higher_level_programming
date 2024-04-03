4fb6cea4457b4474a0e7aa4f46de4c8daabdc0a8
foultrip <davidvasquezmahecha@gmail.com>
2024-04-03 11:53:35 -0500

    mapping 02

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Script with python for connect with MySQL """
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    takes in an argument and displays all values 
+    in the states table of hbtn_0e_0_usa where name 
+    matches the argument.
+    """
+
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    searched = argv[4]
+
+    db = MySQLdb.connect(
+        host=host, port=port, user=user, password=password, db=database
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cur.fetchall()
+    for state in states:
+        if state[1] == searched:
+            print(f"({state[0]}, \'{state[1]}\')")
+    cur.close()
+    db.close()
